cbef7552cbd55e718181833aa92f5234f762c8bb
Josh <mourice1128@gmail.com>
2016-09-11 08:20:42 -0600

    Add files via upload

--- navSwitch.js
+++ navSwitch.js
@@ -35,6 +35,51 @@ $(function(){
    }
 });
 
+$("button").click(function(){
+    $("textarea").select();
+    document.execCommand('copy');
+});
+
+// FOR THE COPY BUTTON 
+
+//(function() {
+
+ // 'use strict';
+
+  // click events
+//  document.body.addEventListener('click', copy, true);
+
+  // event handler
+ // function copy(e) {
+
+    // find target element
+   // var
+   //   t = e.target,
+   //   c = t.dataset.copytarget,
+   //   inp = (c ? document.querySelector(c) : null);
+
+    // is element selectable?
+  //  if (inp && inp.select) {
+
+      // select text
+   //   inp.select();
+
+   //   try {
+        // copy text
+    //    document.execCommand('copy');
+    //    inp.blur();
+     // }
+    //  catch (err) {
+     //   alert('please press Ctrl/Cmd+C to copy');
+    //  }
+
+  //  }
+
+ // }
+
+// })();
+ END \\
+
 	//	function showPopup(url) {
 	//	newwindow=window.open(url,'name','height=300,width=500,top=300,left=500,resizable');
 	//	if (window.focus) {newwindow.focus()}
